0ca4d5efb9ff617c9bf1e2142480f5ab52f0f8fb
totore79 <134659575+totore79@users.noreply.github.com>
2023-10-04 17:19:48 -0300

    Update prod.py
    asfffas

--- config/prod.py
+++ config/prod.py
@@ -6,10 +6,11 @@ FECHA DE CREACIÓN: 08/07/2019
 
 """
 from .default import *
+import os
 
 
-SECRET_KEY = '5e04a4955d8878191923e86fe6a0dfb24edb226c87d6c7787f35ba4698afc86e95cae409aebd47f7'
+SECRET_KEY = os.getenv('secret_key_env')
 
 APP_ENV = APP_ENV_PRODUCTION
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://db_user:db_pass@host:port/db_name'
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
